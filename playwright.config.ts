2111d3a3a6f324723a049211f0492240fc814c2a
simealdana <simeon@wearecapicua.com>
2022-07-18 15:04:59 -0300

    added tesult in github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,12 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 import { devices } from "@playwright/test";
+import 'dotenv/config'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-const TOKEN_TESULT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImQ0ZTE4NGU3LTAyZDEtNGQ2Mi04ZjFlLWQyYThhOTQ3ZGNmYy0xNjU3NjgxMTE3NjA4IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiMDk2NWU1ZjMtOGU5ZS00Nzg1LWFjNWYtMzVhZjEwNTA4Mjc5IiwidHlwZSI6InQifQ.1eVcOOMmCZRS0w9KeUjeA4ewYwvce7XciY0lEaF5Gys'
-const config: PlaywrightTestConfig = {
+const TOKEN_TESULT =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImQ0ZTE4NGU3LTAyZDEtNGQ2Mi04ZjFlLWQyYThhOTQ3ZGNmYy0xNjU3NjgxMTE3NjA4IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiMDk2NWU1ZjMtOGU5ZS00Nzg1LWFjNWYtMzVhZjEwNTA4Mjc5IiwidHlwZSI6InQifQ.1eVcOOMmCZRS0w9KeUjeA4ewYwvce7XciY0lEaF5Gys";
+const defaultConfig: PlaywrightTestConfig = {
   testDir: "./tests",
-  reporter: [['playwright-tesults-reporter', {'tesults-target': TOKEN_TESULT}]],
+
   projects: [
     {
       name: "chromium",
@@ -36,6 +29,17 @@ const config: PlaywrightTestConfig = {
       },
     },
   ],
+};
+
+const tesultsConfig: PlaywrightTestConfig = process?.env?.ENV?.toLowerCase() === 'local' ?{} :{
+  reporter: [
+    ["playwright-tesults-reporter", { "tesults-target": TOKEN_TESULT }],
+  ],
+}
+
+const config: PlaywrightTestConfig = {
+  ...defaultConfig,
+  ...tesultsConfig,
 };
 
 export default config;
